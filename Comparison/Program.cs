13c65da8620be58326146a9670aa9809160c9cc1
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:30:39 -0400

    Add project files.

---
+++ Comparison/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title ="Comparison";
+        int nil = 0, num = 0, max = 1;
+        char cap = 'A', low = 'a';
+        Console.Write("Equality:");
+        Console.Write("\t (0==0) : " + (nil + num));
+        Console.WriteLine("\n\t\t(A==a) : " + (cap == low));
+    }
+}
