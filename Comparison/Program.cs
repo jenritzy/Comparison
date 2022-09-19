7487128ccf48c0562df85eb2d82c12c2091f674e
jenritzy <Jenritzy@outlook.com>
2022-09-19 13:05:38 -0400

    Added remaining comparisons (great-less or equal)

--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -6,7 +6,19 @@
         int nil = 0, num = 0, max = 1;
         char cap = 'A', low = 'a';
         Console.Write("Equality:");
-        Console.Write("\t (0==0) : " + (nil + num));
-        Console.WriteLine("\n\t\t(A==a) : " + (cap == low));
+        Console.Write("\t (0==0) : " + (nil == num));
+        Console.Write("\n\t\t(A==a) : " + (cap == low));
+        Console.Write("\n\nInequality:");
+        Console.Write("\t (0 != 1): " + (nil != max));
+        Console.Write("\n\nGreater: ");
+        Console.Write("\t (0 > 1) : " + (nil > max));
+        Console.Write("\nLess: ");
+        Console.Write("\t\t (0 < 1) : " + (nil < max));
+        Console.Write("\n\nGreater/Equal");
+        Console.Write("\t (0 >= 0)" + (nil >= num));
+        Console.Write("\nLess or equal");
+        Console.Write("\t (1 <= 0)" + (max <= nil));
+        Console.ReadKey();
+
     }
 }
